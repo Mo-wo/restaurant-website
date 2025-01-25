523820e71a414ee45ee79261fc5c14bbf6128c9a
mowo <toryvick20@gmail.com>
2025-01-25 15:14:14 +0100

    Completed menu card

--- src/components/lvl3/menuDescCard/MenuDescCard.tsx
+++ src/components/lvl3/menuDescCard/MenuDescCard.tsx
@@ -1,9 +1,33 @@
-import styles from 'menuDescCard.module.css';
+import styles from './menuDescCard.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-export const MenuDescCard = () => {
+interface MenuDescCardProps {
+  cardStyle: string;
+  cardImgWrapperStyle: string;
+  cardImgSrc: string;
+  cardHeading: string;
+  cardInfo: string;
+  btnText?: string;
+}
+
+export const MenuDescCard: React.FC<MenuDescCardProps> = ({
+  cardStyle, cardImgWrapperStyle, cardImgSrc, cardHeading, cardInfo, btnText
+}) => {
   return (
-    <div role='complementary'>
+    <div role='complementary' className={`${styles.cardWrapper} ${cardStyle}`}>
+      <div className={`${styles.imgWrapper} ${cardImgWrapperStyle}`}>
+        <Image src={cardImgSrc} alt={cardHeading} className={styles.img} />
+      </div>
+      <div role='complementary' className={styles.cardTextWrapper}>
+        <h3 className={styles.cardHeading}>{cardHeading}</h3>
+        <p className={styles.cardInfoText}>{cardInfo}</p>
+      </div>
 
+      <button className={styles.btn}>
+         <Link href='/menu' passHref legacyBehavior><a target='_blank'>View Details</a></Link>
+        </button>
     </div>
   )
 }
--- src/components/pages/home/bestSeller/BestSeller.tsx
+++ src/components/pages/home/bestSeller/BestSeller.tsx
@@ -1,10 +1,37 @@
 import { Section } from '@/components/lvl1/Section/Section';
 import styles from './bestSeller.module.css';
+import { MenuDescCard } from '@/components/lvl3/menuDescCard/MenuDescCard';
+import bestsellerimageone from '@/assets/images/menuItems/bestsellerimageone.svg';
+import bestsellerimagetwo from '@/assets/images/menuItems/bestsellerimagetwo.svg';
+import bestsellerimagethree from '@/assets/images/menuItems/bestsellerimagethree.svg';
 
 export const BestSeller = () => {
   return (
     <Section sectionStyle={styles.bestSeller} color={'white'}>
       <h2>A taste of our best</h2>
+      <div>
+        <MenuDescCard
+          cardStyle={''}
+          cardImgWrapperStyle={''}
+          cardImgSrc={bestsellerimageone}
+          cardHeading={'Jollof Rice'}
+          cardInfo={'A flavorful rice dish cooked with tomatoes, peppers, onions, and spices.'}
+        />
+        <MenuDescCard
+          cardStyle={''}
+          cardImgWrapperStyle={''}
+          cardImgSrc={bestsellerimagetwo}
+          cardHeading={'Pounded Yam and Egusi'}
+          cardInfo={'A smooth, stretchy dough-like dish made from yams, paired with egusi soup, a rich, nutty soup.'}
+        />
+        <MenuDescCard
+          cardStyle={''}
+          cardImgWrapperStyle={''}
+          cardImgSrc={bestsellerimagethree}
+          cardHeading={'Moi Moi'}
+          cardInfo={'A steamed bean pudding made from blended beans, peppers, and onions, often served has a side dish..'}
+        />
+      </div>
     </Section>
   )
 }
