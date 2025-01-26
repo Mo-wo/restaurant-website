17d5e8359e3d22fd994d28780315cbae6283e8a6
mowo <toryvick20@gmail.com>
2025-01-26 03:19:20 +0100

    Preloading best seller image two to avoid layout shift

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -3,10 +3,14 @@ import { BestSeller } from "@/components/pages/home/bestSeller/BestSeller";
 import { MenuPreview } from "@/components/pages/home/menuPreview/MenuPreview";
 import { InteriorPreview } from "@/components/pages/home/interiorPreview/InteriorPreview";
 import { Value } from "@/components/pages/home/value/Value";
+import Head from "next/head";
 
 export default function Home() {
   return(
     <>
+    <Head>
+        <link rel="preload" href="../../assets/images/menuItems/bestsellerimagetwo.svg" as="image" />
+      </Head>
       <Hero />
       <BestSeller />
       <MenuPreview />
