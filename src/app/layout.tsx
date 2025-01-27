bb12e4687a42fc34dcf0e65517112dc805072783
mowo <toryvick20@gmail.com>
2025-01-27 23:57:37 +0100

    Removed unused component from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,6 @@ export default function RootLayout({
             {children}
           </main>
           <Footer />
-          {/* <BackToTop /> */}
           {/* <ThemeSwitcher /> */}
         </ThemeProvider>
       </body>
