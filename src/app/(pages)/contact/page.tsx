93ef8e727c6eea5483848c689e28cae5e224b222
mowo <toryvick20@gmail.com>
2025-02-07 16:13:47 +0100

    Editing contact form

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,11 +1,13 @@
 import { PageBanner } from '@/components/lvl2/banner/pageBanner/PageBanner';
 import {ContactInfo} from '@/components/pages/contact/contactInfo/ContactInfo';
+import ContactForm from '@/components/pages/contact/contactForm/ContactForm';
 
 export default function Contact() {
   return (
     <>
       <PageBanner heading={'WE’D LOVE TO HEAR FROM YOU'} desc={'Reach out to us for reservations, inquiries, or feedback'} />
       <ContactInfo />
+      <ContactForm />
     </>
   );
 }
---
+++ src/components/pages/contact/contactForm/ContactForm.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Section } from '@/components/lvl1/section/Section';
+import styles from './contactForm.module.css';
+import ImageComponent from '@/components/lvl2/image/Image';
+import contactImage from '@/assets/images/contactImage.svg';
+import Button from '@/components/lvl2/button/Button';
+
+export default function ContactForm() {
+  return (
+    <Section color='white' contentContainerStyle={styles.sectionWrapper}>
+      <div className={styles.formContainer}>
+        {/* <div className={styles.imageBg}></div> */}
+        <ImageComponent src={contactImage} alt={``} className={styles.image} />
+        <form className={styles.form}>
+          <h2>Send us a message</h2>
+
+          <div className={styles.inputContainer}>
+
+            <div className={styles.inputWrapper}>
+              <label htmlFor='name' className={styles.label}> Name
+                <input name='name' id='name' type='text' className={styles.input} />
+              </label>
+            </div>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='phone' className={styles.label}>Email
+                <input name='email' id='email' type='email' className={styles.input} />
+              </label>
+            </div>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='phone' className={styles.label}>Phone
+                <input name='phone' id='phone' type='tel' className={styles.input} />
+              </label>
+            </div>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='message' className={styles.label}>Message
+                <textarea cols={30} rows={10} className={styles.input}></textarea>
+              </label>
+            </div>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <div className={styles.ctaSection}>
+        <h2>DON&apos;T JUST BROWSE - INDULGE</h2>
+        <p>Order online for quick and delicious delivery straight to your door</p>
+        <Button href='/gallery/tour' text={'Order Now'} marginTop={'0'} />
+      </div>
+
+
+    </Section>
+  )
+}
