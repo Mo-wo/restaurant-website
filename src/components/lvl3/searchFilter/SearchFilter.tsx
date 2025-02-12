7d6ec64e7f1a9295cb206aaf64a715e6871d63b3
mowo <toryvick20@gmail.com>
2025-02-12 02:52:37 +0100

    Completed search filter component

--- src/components/lvl3/searchFilter/SearchFilter.tsx
+++ src/components/lvl3/searchFilter/SearchFilter.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import styles from './searchFilter.module.css';
 import { IoFilter } from "react-icons/io5";
 
+type SearchFilterProps = {
+  query: string;
+  onSearch: (searchQuery: string) => void;
+  onPriceFilter: (category: string) => void;
+  onPopularFilter: (category: string) => void;
+  onNewestFilter: (category: string) => void;
+}
 
-export const SearchFilter = () => {
+export const SearchFilter:React.FC<SearchFilterProps> = ({ query, onSearch, onPriceFilter, onNewestFilter, onPopularFilter }) => {
+  const [searchQuery, setSearchQuery] = useState<string>(query);
+  const [filtercategory, setFilterCategory] = useState<string>('');
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchQuery(e.target.value);
+    onSearch(searchQuery);
+  }
+
+  const handlePriceFilter = () => {
+    setFilterCategory('price');
+    onPriceFilter(filtercategory);
+  };
+
+  const handlePopularFilter = () => {
+    setFilterCategory('popular');
+    onPopularFilter(filtercategory);
+  };
+  const handleNewestFilter = () => {
+    setFilterCategory('newest');
+    onNewestFilter(filtercategory);
+  };
+
   return (
     <section className={styles.searchSection}>
       <div className={styles.searchWrapper}>
         <BiSearch size={20} />
         <label htmlFor='search'>
-          <input name='search' id='search' type='text' placeholder='Search' className={styles.searchInput} />
+          <input
+            name='search'
+            id='search'
+            type='text'
+            placeholder='Search'
+            className={styles.searchInput}
+            value={searchQuery}
+            onChange={handleSearch}
+          />
         </label>
       </div>
 
       <div className={styles.filterWrapper}>
         <IoFilter size={20} />
         <div className={styles.buttonWrapper}>
-        <button className={styles.textButton}>Price</button>
-        <button className={styles.textButton}>Popular</button>
-        <button className={styles.textButton}>Newest</button>
+        <button className={styles.textButton} onClick={handlePriceFilter}>Price</button>
+        <button className={styles.textButton} onClick={handlePopularFilter}>Popular</button>
+        <button className={styles.textButton} onClick={handleNewestFilter}>Newest</button>
         </div>
         </div>
     </section>
